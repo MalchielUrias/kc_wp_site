12c2b0080c7e9ca08f34b914d6ac0fe5da2bdc03
Malchiel Urias <malchiel.urias@Terra-Admins-MacBook-Pro.local>
2024-12-24 17:20:47 +0100

    ssm to ssh

--- platforms/wordpress_site/main.tf
+++ platforms/wordpress_site/main.tf
@@ -88,7 +88,7 @@ module "github_oidc_policy" {
   policy = data.aws_iam_policy_document.github_access_policy.json
 }
 
-# instance profile
+# Instance Profile
 
 module "instance_profile" {
   source = "github.com/MalchielUrias/tepe_masterclass//modules/iam"
